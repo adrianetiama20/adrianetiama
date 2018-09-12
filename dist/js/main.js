0e75be1c603ccb8b29c7529d4a9a919d50135217
adrianetiama20 <adrianetiama20@gmail.com>
2018-09-11 21:11:35 -1000

    initial

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navitem = document.querySelectorAll('.navitem');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navitem.forEach(items => items.classList.add("show"));
+
+        showMenu = true;
+    } else {
+
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navitem.forEach(items => items.classList.remove('show'));
+
+        showMenu = false;
+
+    }
+}
